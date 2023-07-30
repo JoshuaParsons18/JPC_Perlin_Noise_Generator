9e3680c6935aebd15017c9704e7bb5fb668bd768
Joshua Parsons <joshuaparsons18@gmail.com>
2023-07-29 23:58:51 -0700

    Removed unnecessary files and adding a README.md file.

--- perlin_noise/config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    def __init__(self, seed=None, octaves=1, persistence=0.5, lacunarity=2.0, scale=1.0):
-        self.seed = seed
-        self.octaves = octaves
-        self.persistence = persistence
-        self.lacunarity = lacunarity
-        self.scale = scale
